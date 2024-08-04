4353dd36193cab1d1765d5a43d38c504c8389776
“horia-abdaljabbar” <horiahaji.77sr@gmail.com>
2024-08-04 10:19:01 +0300

    createProductsTable

---
+++ EF_project/Context/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using EF_project.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_project.Context
+{
+    internal class ApplicationDbContext :DbContext
+
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=ASP9_EF;Trusted_Connection=True;TrustServerCertificate=True");
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
